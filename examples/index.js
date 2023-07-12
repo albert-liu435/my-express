f1c5aba1e51862651b7f5caaae56f1c868ee3f56
liuxili01 <liuxili@bestseller.com.cn>
2023-07-12 15:47:54 +0800

    day04

--- examples/index.js
+++ examples/index.js
@@ -1,15 +1,74 @@
 const express = require('../index.js')
 const app = express()
+let pathJson = {}
 app.listen(3000) // 启动端口为3000的服务
 // localhost:3000/path 时调用
-app.get('/path', function (req, res) {
+app.get('/path', function (req, res, next) {
   console.log('visite /path , send : path')
-  res.end('path')
+  // res.end('path')
+  pathJson.index = 1
+  next()
+})
+// localhost:3000/path 时调用，先走第一个，再走这个
+app.get('/path', function (req, res) {
+  pathJson.end = true
+  res.end(JSON.stringify(pathJson))
 })
 // localhost:3000/ 时调用
 app.get('/', function (req, res) {
-  console.log('visite /, send: root')
   res.end('root')
 })
+// 发生post请求的时候调用
+app.post('/post/path', function (req, res) {
+  res.end('post path')
+})
+// 3:新增 输出传入的id，和name时拦截处理参数
+app.param([
+  'id', 'name'
+], function (req, res, next, val, name) {
+  if (name == 'id') {
+    req.params.id = ((val - 0) + 3) + ''
+  }
+  if (name == 'name') {
+    req.params[name] = req.params[name] + ' param'
+  }
+  next()
+})
+// 3:新增 当路径为/get 时拦截处理query
+app.use('/get', function (req, res, next) {
+  for (key in req.query) {
+    req.query[key] = req.query[key] + ' use'
+  }
+  next()
+})
 
-//exports = module.exports = app
+// 测试param处理id ,name
+app.post('/user/:id/:name', function (req, res) {
+  res.end(JSON.stringify(req.params))
+})
+
+// 测试param处理id
+app.post('/user/:id', function (req, res) {
+  res.end(JSON.stringify(req.params))
+})
+
+// 测试param处理name
+app.post('/name/:name', function (req, res) {
+  res.end(JSON.stringify(req.params))
+})
+
+app.get('/get', function (req, res) {
+  res.end(JSON.stringify(req.query))
+})
+
+let router = express.Router({mergeParams: true})
+router.get('/getname/:like', function (req, res, next) {
+  res.end(JSON.stringify(req.params))
+})
+app.use('/:userId', router)
+let subApp = express()
+subApp.get('/getuser', function (req, res, next) {
+  res.end(JSON.stringify(req.params))
+})
+app.use('/sub/:id', subApp)
+exports = module.exports = app
--- test/index.js
+++ test/index.js
@@ -17,7 +17,7 @@ describe('服务器测试', () => {
         done()
       })
   })
-  // 如果走的不是examples中的get：/path 测试不通过
+  // 如果走的不是examples中的get：/path 测试不通过;
   it('GET /path', (done) => {
     request
       .get('/path')
@@ -25,7 +25,126 @@ describe('服务器测试', () => {
       .end((err, res) => {
         if (err) 
           return done(err)
-        assert.equal(res.text, 'path', 'res is wrong') // 根据response调用end方法时的输出为: path
+        let json = JSON.parse(res.text)
+        assert.equal(json.index, 1, 'didn`t visite the first route /path') // 查看是否调用了第一次的注册
+        assert.equal(json.end, true, 'res is wrong') // 查看是否调用了第二次注册
+        done()
+      })
+  })
+  // 如果走的不是examples中的post：/post/path 测试不通过
+  it('POST /post/path', (done) => {
+    request
+      .post('/post/path')
+      .expect(200)
+      .end((err, res) => {
+        if (err) 
+          return done(err)
+        assert.equal(res.text, 'post path', 'res is wrong') // 根据response调用end方法时的输出为: post path
+        done()
+      })
+  })
+
+  // 测试get: /get 带query
+  it('GET /get?test=once', (done) => {
+    request
+      .get('/get?test=once')
+      .expect(200)
+      .end((err, res) => {
+        if (err) 
+          return done(err)
+        let params = JSON.parse(res.text)
+        assert.equal(params.test, 'once use', 'res.text must has prototype test and the value must be once use') // 经过use方法处理后的test为once+ use = once use
+        done()
+      })
+  })
+
+  // 如果走的不是examples中的post：/user/:id/:name 测试不通过
+  it('POST /user/12/kaisela', (done) => {
+    request
+      .post('/user/12/kaisela')
+      .expect(200)
+      .end((err, res) => {
+        if (err) 
+          return done(err)
+        let params = JSON.parse(res.text)
+        assert.equal(params.id, '15', 'id must be 15') // 经过param方法处理后的id为12+3 = 15
+        assert.equal(params.name, 'kaisela param', 'name must be kaisela param')
+        // 经过param方法处理后的id为kaisela+ param = kaisela param
+        done()
+      })
+  })
+
+  // 如果走的不是examples中的post：/user/:id测试不通过
+  it('POST /user/17', (done) => {
+    request
+      .post('/user/17')
+      .expect(200)
+      .end((err, res) => {
+        if (err) 
+          return done(err)
+        let params = JSON.parse(res.text)
+        // 经过param方法处理后的id为17+3 = 20
+        assert.equal(params.id, '20', 'id must be 20')
+        done()
+      })
+  })
+
+  // 如果走的不是examples中的post：/name/:name测试不通过
+  it('POST /name/ke', (done) => {
+    request
+      .post('/name/ke')
+      .expect(200)
+      .end((err, res) => {
+        if (err) 
+          return done(err)
+        let params = JSON.parse(res.text)
+        // 经过param方法处理后的id为ke+ param = ke param
+        assert.equal(params.name, 'ke param', 'name must be ke param')
+        done()
+      })
+  })
+
+  // 如果走的不是examples中的post：/user/:id/:name 测试不通过
+  /* it('POST /user/12/user', (done) => {
+    request
+      .post('/user/12/user')
+      .expect(200)
+      .end((err, res) => {
+        console.log(res)
+        if (err) {
+          if (err === 'error') {
+            return done()
+          }
+        }
+        done()
+      })
+  }) */
+
+  // 4:新增 测试get: /:userId/getname/:like
+  it('GET  /:userId/getname/:like', (done) => {
+    request
+      .get('/12/getname/ll')
+      .expect(200)
+      .end((err, res) => {
+        if (err) 
+          return done(err)
+        let params = JSON.parse(res.text)
+        assert.equal(params.userId, '12', 'res.text must has prototype userId and the value must be 12') // 经过use方法处理后的test为once+ use = once use
+        assert.equal(params.like, 'll', 'res.text must has prototype like and the value must be ll')
+        done()
+      })
+  })
+
+  // 4:新增 测试get: /:userId/getname/:like
+  it('GET /sub/:id/getuser', (done) => {
+    request
+      .get('/sub/13/getuser')
+      .expect(200)
+      .end((err, res) => {
+        if (err) 
+          return done(err)
+        let params = JSON.parse(res.text)
+        assert.equal(params.id, '16', 'res.text must has prototype id and the value must be 13')
         done()
       })
   })
