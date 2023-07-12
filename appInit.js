0d047c4ec09b25a1437408a75c82fae63553bfcc
liuxili01 <liuxili@bestseller.com.cn>
2023-07-12 17:34:39 +0800

    day01

--- appInit.js
+++ appInit.js
@@ -6,3 +6,15 @@ function init() {
 }
 
 init();
+
+
+let app={}
+app._init = function init() {
+  //用来保存一些环境变量的设置信息
+  // this.setting = {};
+  // this.paths = [];
+  console.log("_init");
+
+};
+
+app._init();
